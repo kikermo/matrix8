c3677402563103bb3eee88eceab505dc505d7231
Enrique Ramirez <kikermo@gmail.com>
2024-06-04 20:12:35 +0100

    Simplify di

--- composeApp/src/desktopMain/kotlin/org/kikermo/matrix8/di/Matrix8Module.kt
+++ composeApp/src/desktopMain/kotlin/org/kikermo/matrix8/di/Matrix8Module.kt
@@ -5,20 +5,14 @@ import org.kikermo.matrix8.domain.SetMatrix8UseCase
 import org.kikermo.matrix8.io.Matrix8I2CPeripheralFactory
 import org.kikermo.matrix8.presentation.Matrix8ViewModel
 import org.kikermo.matrix8.repository.persistence.MatrixPersister
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
+
 val matrix8Module = module {
-//    single { Matrix8I2CPeripheralFactory().create() }
-//    singleOf(::SetMatrix8UseCase)
-//    singleOf(::GetMatrix8PedalsUseCase)
-//    singleOf(::Matrix8ViewModel)
-    single {
-        Matrix8ViewModel(
-            setMatrix8UseCase = SetMatrix8UseCase(
-                matrixPersister = MatrixPersister(),
-                i2CPeripheral = Matrix8I2CPeripheralFactory().create()
-            ),
-            getMatrix8Pedals = GetMatrix8PedalsUseCase()
-        )
-    }
+    single { Matrix8I2CPeripheralFactory().create() }
+    singleOf(::SetMatrix8UseCase)
+    singleOf(::MatrixPersister)
+    singleOf(::GetMatrix8PedalsUseCase)
+    singleOf(::Matrix8ViewModel)
 }
