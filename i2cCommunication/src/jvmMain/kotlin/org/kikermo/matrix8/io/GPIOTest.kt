6c3974aa4047b8c85ffbc45ef4eb42726084201b
Enrique Ramirez <kikermo@gmail.com>
2024-06-19 22:20:33 +0100

    Add gpio example

---
+++ i2cCommunication/src/jvmMain/kotlin/org/kikermo/matrix8/io/GPIOTest.kt
@@ -0,0 +1,27 @@
+package org.kikermo.matrix8.io
+
+import com.pi4j.Pi4J
+import com.pi4j.io.IOType
+import com.pi4j.io.gpio.digital.DigitalOutputConfig
+import com.pi4j.io.gpio.digital.DigitalOutputProvider
+import com.pi4j.platform.Platform
+
+
+class GPIOTest {
+
+    companion object {
+        private const val DIGITAL_OUTPUT_PIN: Int = 3
+    }
+
+    private val pi4j = Pi4J.newAutoContext()
+    private val platform: Platform = pi4j.platforms().getDefault()
+    private val provider: DigitalOutputProvider = platform.provider(IOType.DIGITAL_OUTPUT)
+    private val config = DigitalOutputConfig.newBuilder(pi4j).address(DIGITAL_OUTPUT_PIN).build()
+    private val output = provider.create(config)
+
+    fun setLED(boolean: Boolean) {
+
+    }
+
+
+}
