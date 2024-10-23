07890f7338cb1971f8320c64df7a60f22072bfee
Enrique Ramirez <kikermo@gmail.com>
2024-10-23 13:40:08 +0100

    Bring back i2c

--- composeApp/src/desktopMain/kotlin/org/kikermo/matrix8/main.kt
+++ composeApp/src/desktopMain/kotlin/org/kikermo/matrix8/main.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.launch
 import org.kikermo.matrix8.di.matrix8Module
 import org.kikermo.matrix8.io.Matrix8BleService
 import org.kikermo.matrix8.io.Matrix8GPIOService
+import org.kikermo.matrix8.io.Matrix8I2CService
 import org.koin.core.context.startKoin
 import java.lang.Thread.sleep
 
@@ -42,7 +43,7 @@ private fun initApp() {
     System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "INFO")
     startKoin {
         modules(matrix8Module)
-        //    val i2cServer: Matrix8I2CService = koin.get()
+        val i2cServer: Matrix8I2CService = koin.get()
         val gpioService: Matrix8GPIOService = koin.get()
         val bleService: Matrix8BleService = koin.get()
         CoroutineScope(Dispatchers.IO).launch {
